ce44798c5c1eb6a64645c19a5fbc17bc09c49aca
Rupak <rpkc.mail@gmail.com>
2021-01-23 20:30:48 +0530

    Update index.js
    Text update

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   var typed = new Typed('#description', {
-      strings: [ 'Hello','I am <br>^300Rupak <br>^300Chowdhury',' I am a <br> Hobbyist <br> Programmer <br>^2000 ','I am a <br> Learner^2000'],
+      strings: [ 'Hello','I am <br>Rupak <br>Chowdhury^2000',' I am a <br> Hobbyist <br> Programmer <br>^2000 ','I am a <br> Learner^2000'],
       typeSpeed: 100,
       backSpeed: 100,
       loop:true,
